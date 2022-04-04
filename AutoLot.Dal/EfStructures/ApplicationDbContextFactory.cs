a42870be8a38a190410c1e5715f2714092604ab1
dez <mega6ban@mail.ru>
2022-04-05 00:55:51 +0300

    First migrations (db initialize)

---
+++ AutoLot.Dal/EfStructures/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace AutoLot.Dal.EfStructures
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(GetDbConnectionString());
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+
+        private string GetDbConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(@"C:\FEFPractice\")
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            return config["connectionString"];
+        }
+    }
+}

6bb48b94e6c793b26914a038c94a70e46d62845c
dez <mega6ban@mail.ru>
2022-04-05 04:06:15 +0300

    Exceptions and Events added

--- AutoLot.Dal/EfStructures/ApplicationDbContext.cs
+++ AutoLot.Dal/EfStructures/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using AutoLot.Models.Entities.Owned;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using AutoLot.Dal.Exceptions;
 
 namespace AutoLot.Dal.EfStructures
 {
@@ -15,19 +16,75 @@ namespace AutoLot.Dal.EfStructures
         {
         }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            ChangeTracker.StateChanged += ChangeTracker_StateChanged;
+            ChangeTracker.Tracked += ChangeTracker_Tracked;
+
+            base.SavingChanges += (sender, args) =>
+            {
+                Console.WriteLine($"Saving changes for {((ApplicationDbContext)sender)!.Database!.GetConnectionString()}");
+            };
+
+            base.SavedChanges += (sender, args) =>
+            {
+                Console.WriteLine($"Saved {args!.EntitiesSavedCount} changes for {((ApplicationDbContext)sender)!.Database!.GetConnectionString()}");
+            };
+            base.SaveChangesFailed += (sender, args) =>
+            {
+                Console.WriteLine($"An exception occurred! {args.Exception.Message} entities");
+            };
         }
 
-        public virtual DbSet<CreditRisk> CreditRisks { get; set; } = null!;
-        public virtual DbSet<Customer> Customers { get; set; } = null!;
-        public virtual DbSet<Car> Cars { get; set; } = null!;
-        public virtual DbSet<Make> Makes { get; set; } = null!;
-        public virtual DbSet<Order> Orders { get; set; } = null!;
+        private void ChangeTracker_StateChanged(object? sender, EntityStateChangedEventArgs e)
+        {
+            if (e.Entry.Entity is not Car c)
+            {
+                return;
+            }
+
+            var action = string.Empty;
+            Console.WriteLine($"Car {c.PetName} was {e.OldState} before the state changed to {e.NewState}");
+            
+            switch (e.NewState)
+            {
+                case EntityState.Unchanged:
+                    action = e.OldState switch
+                    {
+                        EntityState.Added => "Added",
+                        EntityState.Modified => "Edited",
+                        _ => action
+                    };
+
+                    Console.WriteLine($"The object was {action}");
+                    break;
+            }
+        }
+
+        private void ChangeTracker_Tracked(object? sender, EntityTrackedEventArgs e)
+        {
+            var source = (e.FromQuery) ? "Database" : "Code";
+
+            if (e.Entry.Entity is Car c)
+            {
+                Console.WriteLine($"Car entry {c.PetName} was added from {source}");
+            }
+        }
+
+        public DbSet<CreditRisk>? CreditRisks { get; set; } = null!;
+        public DbSet<Customer>? Customers { get; set; } = null!;
+        public DbSet<Car>? Cars { get; set; } = null!;
+        public DbSet<Make>? Makes { get; set; } = null!;
+        public DbSet<Order>? Orders { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<SeriLogEntry>(entity =>
+            {
+                entity.Property(e => e.Properties).HasColumnName("Xml");
+                entity.Property(e => e.TimeStamp).HasDefaultValueSql("GETDATE()");
+            });
+
             modelBuilder.Entity<CreditRisk>(entity =>
             {
                 entity.OwnsOne(owns => owns.PersonalInformation, navigation =>
@@ -63,17 +120,9 @@ namespace AutoLot.Dal.EfStructures
 
             modelBuilder.Entity<Car>(entity =>
             {
-                entity.ToTable("Inventory");
+                entity.HasQueryFilter(c => c.IsDrivable);
 
-                entity.HasIndex(e => e.MakeId, "IX_Inventory_MakeId");
-
-                entity.Property(e => e.Color).HasMaxLength(50);
-
-                entity.Property(e => e.PetName).HasMaxLength(50);
-
-                entity.Property(e => e.TimeStamp)
-                    .IsRowVersion()
-                    .IsConcurrencyToken();
+                entity.Property(p => p.IsDrivable).HasField("_isDrivable").HasDefaultValue(true);
 
                 entity.HasOne(d => d.MakeNavigation)
                     .WithMany(p => p.Cars)
@@ -84,33 +133,27 @@ namespace AutoLot.Dal.EfStructures
 
             modelBuilder.Entity<Make>(entity =>
             {
-                entity.Property(e => e.Name).HasMaxLength(50);
-
-                entity.Property(e => e.TimeStamp)
-                    .IsRowVersion()
-                    .IsConcurrencyToken();
+                entity.HasMany(e => e.Cars)
+                .WithOne(c => c.MakeNavigation)
+                .HasForeignKey(c => c.MakeId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_Make_Inventory");
             });
 
             modelBuilder.Entity<Order>(entity =>
             {
-                entity.HasIndex(e => e.CarId, "IX_Orders_CarId");
-
-                entity.HasIndex(e => new { e.CustomerId, e.CarId }, "IX_Orders_CustomerId_CarId")
-                    .IsUnique();
-
-                entity.Property(e => e.TimeStamp)
-                    .IsRowVersion()
-                    .IsConcurrencyToken();
+                entity.HasQueryFilter(e => e.CarNavigation!.IsDrivable);
 
                 entity.HasOne(d => d.CarNavigation)
-                    .WithMany(p => p.Orders)
+                    .WithMany(p => p!.Orders)
                     .HasForeignKey(d => d.CarId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_Orders_Inventory");
 
                 entity.HasOne(d => d.CustomerNavigation)
-                    .WithMany(p => p.Orders)
+                    .WithMany(p => p!.Orders)
                     .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.Cascade)
                     .HasConstraintName("FK_Orders_Customers");
             });
 
@@ -118,5 +161,35 @@ namespace AutoLot.Dal.EfStructures
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                //A concurrency error occurred
+                //Should log and handle intelligently
+                throw new CustomConcurrencyException("A concurrency error happened.", ex);
+            }
+            catch (RetryLimitExceededException ex)
+            {
+                //DbResiliency retry limit exceeded
+                //Should log and handle intelligently
+                throw new CustomRetryLimitExceededException("There is a problem with SQl Server.", ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                //Should log and handle intelligently
+                throw new CustomDbUpdateException("An error occurred updating the database", ex);
+            }
+            catch (Exception ex)
+            {
+                //Should log and handle intelligently
+                throw new CustomException("An error occurred updating the database", ex);
+            }
+        }
     }
 }
